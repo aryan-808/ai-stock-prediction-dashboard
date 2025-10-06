34f8d7b19ec1ea989e968f8ea9d0163f23f66de9
quant_x.py <mrdisspute666@gmail.com>
2025-10-06 23:36:13 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,8 +14,7 @@ const eslintConfig = [
     rules: {
       'react/no-unescaped-entities': 'off',
       '@next/next/no-img-element': 'off',
-      '@typescript-eslint/no-unused-vars': 'off',
-      '@typescript-eslint/no-explicit-any': 'off',
+      '@next/next/no-page-custom-font': 'off',
       'react-hooks/exhaustive-deps': 'off',
       'import/no-unresolved': 'error',
       'import/named': 'error',
